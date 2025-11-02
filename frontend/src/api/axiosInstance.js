a0f73db8d704a34b348d9abc0486558339c1c682
dps3007 <deepanshupal30072002@gmail.com>
2025-11-03 00:48:53 +0530

    frd0

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
-export const BACKEND_URL =
-  import.meta.env.VITE_BACKEND_URL || "http://localhost:8000";
+export const BACKEND_URL = "https:url-shortner-backend-irr2.onrender.com";
 
-const axiosInstance = axios.create({ baseURL: BACKEND_URL});
+const axiosInstance = axios.create({ baseURL: BACKEND_URL });
 
 axiosInstance.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
