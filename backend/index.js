caab35ae1b49afcee67d6dbdba52c44738a6db2c
dps3007 <deepanshupal30072002@gmail.com>
2025-11-02 23:11:11 +0530

    backend ready

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
-import { autherizationMiddleware } from "./middleware/auth.middleware.js";
+
 import userRouter from "./routes/user.routes.js";
 import urlRouter from "./routes/url.routes.js";
 
@@ -10,7 +10,7 @@ const PORT = process.env.PORT ?? 8000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true,
   })
 );
